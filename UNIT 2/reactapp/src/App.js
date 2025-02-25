593ed61fa0f59e9f86cc55eeefb0584ad3c2d80e
Saimadhav-333 <saimadhav9235@gmail.com>
2025-02-25 10:01:29 +0530

    portfolio and feedback with cdn

---
+++ UNIT 2/reactapp/src/App.js
@@ -0,0 +1,12 @@
+import Saimadhav from './components/Saimadhav';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Saimadhav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ UNIT 2/reactapp/src/components/Saimadhav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Saimadhav = () => {
+  return (
+    <div>
+      <h3>This is SaiMadhav</h3>
+      <h2>From KMIT Collage</h2>
+    </div>
+  )
+}
+
+export default Saimadhav
